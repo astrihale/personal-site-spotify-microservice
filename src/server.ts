2e12e3191342b966d2a1513355d729c1ea21c805
astrihale <nanarietta@gmail.com>
2023-02-15 14:57:19 +0100

    Okay, configured now everything even better

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import express, {Express, Request, Response} from 'express';
-import {config as ConfigEnvironment} from 'dotenv';
+import {name} from '../package.json';
+import dotenv from 'dotenv';
+import pino from 'pino';
 
-// Load environment variables from development.env file.
-ConfigEnvironment();
-
-const application: Express = express();
+// Load environment variables from .env file.
+dotenv.config();
 const PORT: Number = Number(process.env.PORT) || 7689;
 
+// Create the main application
+const application: Express = express();
+const logger = pino();
+
+// Add the default echo route
 application.get('/', async (req: Request, res: Response) => {
-    res.status(200).send('Hello from spotify-microservice!');
+    res.status(200).send({message: `Hello from '${name}'!`});
 });
 
+// Start the application
 application.listen(PORT, () => {
-    console.log(`Started server on port: ${PORT}.`);
+    logger.info(`Started server on port: ${PORT}.`);
 });
