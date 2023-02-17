363bd79ae6b7628e3661f7e9333f8725cc76e23c
astrihale <nanarietta@gmail.com>
2023-02-17 11:26:02 +0100

    Finally made the first version of this that works, now I need to secure the endpoints, and store the tokens in redis

---
+++ src/routes/spotify.ts
@@ -0,0 +1,96 @@
+import {Request, Response, Router} from "express";
+import SpotifyWebApi from 'spotify-web-api-node';
+import pino from 'pino';
+
+const spotifyRouter = Router();
+const logger = pino({level: 'debug'});
+
+// Create the spotify client
+const spotifyAPI = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.SPOTIFY_REDIRECT_URL
+});
+
+// Make place for the access token
+const scopes = ['user-read-currently-playing'];
+const state = 'big-man-little-dignity';
+const token = {
+    accessToken: '',
+    refreshToken: '',
+    expiresIn: 0
+};
+
+// Create the method for obtaining, checking and refreshing the access token
+const tokenHandler = async () => {
+    // Check the access token and expires in
+    if (token.accessToken.length > 0 && new Date(token.expiresIn) > new Date()) {
+        return true;
+    }
+
+    // Refresh the token
+    try {
+        let grant = await spotifyAPI.refreshAccessToken();
+        token.accessToken = grant.body['access_token'];
+        spotifyAPI.setAccessToken(token.accessToken);
+        return true;
+    } catch (error) {
+        logger.error('Failed to obtain to refresh the token!');
+        return false;
+    }
+};
+
+// Create the endpoint for logging in
+spotifyRouter.get('/music-auth', async (req, res) => {
+    /*
+        #swagger.tags = ['Music']
+     */
+
+    res.redirect(spotifyAPI.createAuthorizeURL(scopes, state));
+});
+
+// Take received code and get the authorization code grant
+spotifyRouter.get('/music-cred', async (req, res) => {
+    /*
+        #swagger.tags = ['Music']
+     */
+
+    try {
+        const code = req.query['code'] as string;
+        let grant = await spotifyAPI.authorizationCodeGrant(code);
+        token.accessToken = grant.body['access_token'];
+        token.refreshToken = grant.body['refresh_token'];
+        token.expiresIn = grant.body['expires_in'];
+        spotifyAPI.setAccessToken(token.accessToken);
+        spotifyAPI.setRefreshToken(token.refreshToken);
+        return res.status(200).render('SpotifyUpdated');
+    } catch (error) {
+        logger.error('Failed to obtain an Authorization Code Grant.');
+        return res.status(500).render('SpotifyFailedToUpdate');
+    }
+});
+
+// Create the endpoint for current playing track
+spotifyRouter.get('/music', async (req, res) => {
+    /*
+        #swagger.tags = ['Music']
+     */
+
+    if (!await tokenHandler()) {
+        return res.status(500).send('Failed to handle token!');
+    }
+
+    try {
+        let data = await spotifyAPI.getMyCurrentPlayingTrack();
+        res.status(data.statusCode).send(data.body);
+    } catch (error) {
+        res.status(500).send({
+            message: 'Experienced an error while obtaining current' +
+                ' playing track.',
+            data: error
+        });
+    }
+});
+
+// Return the router
+export = spotifyRouter;
--- src/server.ts
+++ src/server.ts
@@ -10,18 +10,26 @@ const PORT: Number = Number(process.env.PORT) || 7689;
 
 // Create the main application
 const application: Express = express();
+application.set('view engine', 'ejs');
 const logger = pino({
     level: 'debug'
+});
+
+// Create the primitive request/response logger
+application.use(async (req: Request, res: Response, next: NextFunction) => {
+    next();
+    logger.debug(`${req.method} ${req.originalUrl} - ${res.statusCode}`);
 });
 
 // Add the echo router
 import echoRouter from "./routes/echo";
 
-application.use(async (req: Request, res: Response, next: NextFunction) => {
-    next();
-    logger.debug(`${req.method} ${req.path} - ${res.statusCode}`);
-});
 application.use('/', echoRouter);
+
+// Add the spotify router
+import spotifyRouter from "./routes/spotify";
+
+application.use('/', spotifyRouter);
 
 // Add the swagger endpoint
 import {generateOpenApi} from "./swagger";
@@ -29,6 +37,20 @@ import {generateOpenApi} from "./swagger";
 setTimeout(async () => {
     application.use('/api-docs/', SwaggerServe, SwaggerSetup(require(await generateOpenApi())));
     application.get('/', async (req: Request, res: Response) => res.redirect(`${req.protocol}://${req.get('host')}${req.originalUrl}api-docs/`));
+
+    application.use(async (req: Request, res: Response, next: NextFunction) => {
+        res.status(404);
+        if (req.accepts('html')) {
+            res.render('404');
+        } else if (req.accepts('json')) {
+            res.send({
+                message: `We're sorry, but we failed to find what you're looking for.`,
+                code: `PAGE_NOT_FOUND`
+            });
+        } else {
+            res.send('PAGE_NOT_FOUND');
+        }
+    });
 
     // Start the application
     application.listen(PORT, () => {
